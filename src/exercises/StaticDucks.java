9aa3711c5e9a6ac193c768c2a23b9eb1ad7f1622
jevticmarina89 <marina.jevtic89@gmail.com>
2019-05-19 23:29:58 +0200

    Testing Duck objects

--- src/exercises/StaticDucks.java
+++ src/exercises/StaticDucks.java
@@ -5,24 +5,27 @@ import junit.framework.TestCase;
 
 public class StaticDucks extends TestCase {
 
-//	public void testWeCanCountTheNumberOfDucksCreated() throws Exception {
-//		assertEquals(0, Duck.getNumberOfCreations());
-//		Duck duck1 = new Duck();
-//		Duck duck2 = new Duck();
-//		Duck duck3 = new Duck();
-//		assertEquals(3, Duck.getNumberOfCreations());
-//		Duck duck4 = new Duck();
-//		assertEquals(4, Duck.getNumberOfCreations());
-//	}
-//
-//	public void testEverySecondDuckIsBlack() throws Exception {
-//		Duck duck1 = new Duck();
-//		Duck duck2 = new Duck();
-//		Duck duck3 = new Duck();
-//		assertEquals("black", duck1.getColor());
-//		assertEquals("white", duck2.getColor());
-//		assertEquals("black", duck3.getColor());
-//	}
+	public void testWeCanCountTheNumberOfDucksCreated() throws Exception {
+		assertEquals(0, Duck.getNumberOfCreations());
+		Duck duck1 = new Duck("worm", 50, "black");
+		Duck duck2 = new Duck("apples", 70, "white");
+		Duck duck3 = new Duck("bugs", 30, "black");
+		assertEquals(3, Duck.getNumberOfCreations());
+		Duck duck4 = new Duck("bread", 20, "white");
+		assertEquals(4, Duck.getNumberOfCreations());
+		duck1.reset();
+	}
+	
+
+	public void testEverySecondDuckIsBlack() throws Exception {
+		Duck duck1 = new Duck("worm", 50, "black");
+		Duck duck2 = new Duck("apple", 70, "white");
+		Duck duck3 = new Duck("bug", 30, "black");
+		assertEquals("black", duck1.getColor());
+		assertEquals("white", duck2.getColor());
+		assertEquals("black", duck3.getColor());
+		duck1.reset();
+	}
 //
 //	public void testEveryFourJellyfishArePoisonous() throws Exception {
 //	}
